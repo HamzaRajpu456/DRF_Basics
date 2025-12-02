4971123a414976f9dd297a31f998f0fe0236b91e
HamzaRajpu456 <hamzarajput7896@gmail.com>
2025-12-02 20:04:12 +0500

    create drf project by using django-admin command then create a model name item contains name and price and put db_info into settings of project , cretae serializer for converting data,and then  create tables in db using migrations.

---
+++ api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Item
+
+class ItemSerializer(serializers.Serializer):
+
+    class Meta :
+        model = Item
+        fields = '__all__'
+        
