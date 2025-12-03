0641d5471e624867554c9da641c2e5a8104ecace
HamzaRajpu456 <hamzarajput7896@gmail.com>
2025-12-03 17:25:16 +0500

    create auto GenricViews of category

--- generic_views/views.py
+++ generic_views/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
 
-# Create your views here.
+
+# GenericViews (Auto):
+
+# Create Category CRUD:
+# Create & List of Category:
+
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+# Retrieve, Upate and Delete Category:
+
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    
