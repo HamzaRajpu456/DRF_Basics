6f15396025207bf932dccf7ad34f7067eccc4f95
HamzaRajpu456 <hamzarajput7896@gmail.com>
2025-12-03 19:06:03 +0500

    write urls of products and test to create and list , detail of products and categories

--- DRF_project/urls.py
+++ DRF_project/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('api_2/', include('generic_views.urls'))
+    path('generic/', include('generic_views.urls'))
     
 ]
--- generic_views/urls.py
+++ generic_views/urls.py
@@ -1,10 +1,14 @@
-from .views import CategoryListCreateView, CategoryDetailView
+from .views import CategoryListCreateView, CategoryDetailView, ProductListCreateView, ProuctDetailView
 from django.urls import path
 
 
 urlpatterns = [
 
-  path('categories/', CategoryListCreateView.as_view(), name='category-list'),
-  path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
+  path('categories/', CategoryListCreateView.as_view()),
+  path('categories/<int:pk>/', CategoryDetailView.as_view()),
+  
+  
+  path('products/', ProductListCreateView.as_view()),
+  path('products/<int:pk>', ProuctDetailView.as_view()),
 
 ]
