04cdfa8c78c19b100e5ca97166a055c7e87453e5
HamzaRajpu456 <hamzarajput7896@gmail.com>
2025-12-04 20:11:22 +0500

    cretae views/manually CRUD API using APIVIEW in DRF, use list and detail API

--- manual_views/views.py
+++ manual_views/views.py
@@ -1,3 +1,68 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView 
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
+
+# Perfrom CRUD Manually :
+
+
+
+class ProductListCreateAPI(APIView):
+
+    def get(self, request):
+       
+        product = Product.objects.all()
+        serializer = ProductSerializer(product, many = True) 
+        return Response (serializer.data)
+        
+    def post(self, request):
+        
+        serializer = ProductSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+
+
+
+class ProductDetailAPI(APIView):
+
+    def get_object(self, id):
+        try:
+            return Product.objects.get(id = id)
+        except Product.DoesNotExist:
+            return None
+        
+    def get(self, request, id):
+        product = self.get_object(id)
+        if not product:
+            return Response({"Error":"Not Found"})
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    
+
+    def put(self,request,id):
+        product = Product.objects.get(id)
+        if not product:
+            return Response({"Error":"Product not Found!"})
+        
+        serializer = ProductSerializer(product, data = request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+    def delete(self,request,id):
+        product = self.get_object(id)
+        
+        if not product:
+            return Response({"error":"Product not found!"})
+        
+        product.delete()
+        return Response({"Message":"Product Deleted Successfully"})
+    
